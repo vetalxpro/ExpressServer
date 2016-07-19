0126dc4521089183907494092ac2f9657b06d595
vetalxpro <vetalpro.exe@gmail.com>
2016-07-19 06:47:55 +0100

    sort table

--- public/dom/events.js
+++ public/dom/events.js
@@ -234,5 +234,88 @@ function start() {
   	target.classList.toggle('mark');
   }
 
+// ============================sort table
+
+  let ageTable  = document.getElementById('grid');
+  ageTable.onclick=function(event){
+    let target = event.target;
+    let comparator;
+    let cellNumber = target.cellIndex;
+    let tbody = ageTable.tBodies[0];
+    let tbodyRowsArr = [];
+
+    if(target.tagName!='TH') return;
+
+    if(!target.dataset.sort){
+      target.setAttribute('data-sort','a-z');
+    }
+
+    for(let i=0;i<ageTable.tHead.rows[0].cells.length;i++){
+      ageTable.tHead.rows[0].cells[i].classList.remove('a-z','z-a');
+    }
+
+
+    for(let i =0;i<tbody.rows.length;i++){
+      let rows = tbody.rows;
+      tbodyRowsArr.push({
+        row:rows[i],
+        value:rows[i].children[cellNumber].innerHTML
+      });
+    }
+
+    switch (target.dataset.sort){
+      case 'a-z':
+        target.setAttribute('data-sort','z-a');
+        target.classList.add('a-z');
+        if(target.dataset.type=='number'){
+          comparator = function(rowA,rowB){
+            return rowA.value-rowB.value;
+          };
+        }else{
+          comparator = function(rowA,rowB){
+            if(rowA.value>rowB.value)return 1;
+            return -1;
+          };
+        }
+        break;
+      case 'z-a':
+        target.classList.add('z-a');
+        target.setAttribute('data-sort','a-z');
+        if(target.dataset.type=='number'){
+          comparator = function(rowA,rowB){
+            return rowB.value-rowA.value;
+          };
+        }else{
+          comparator=function(rowA,rowB){
+            if(rowA.value>rowB.value)return -1;
+            return 1;
+          };
+        }
+        break;       
+    }
+
+    tbodyRowsArr.sort(comparator);
+
+    ageTable.removeChild(tbody);
+
+    for(let i=0;i<tbodyRowsArr.length;i++){
+      tbody.appendChild(tbodyRowsArr[i].row);
+    }
+
+    ageTable.appendChild(tbody);
+
+    // function createComparator(dataType,dataSort){
+    //   if(dataType == 'number') return function(a,b){
+    //     return 
+    //   }
+    // }
+
+    
+  }
+  
+  
+
+
+
 
 }
