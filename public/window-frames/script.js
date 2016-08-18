f4fd517d4830f89ebafea1dcb514548f3017064b
vetalxpro <vetalpro.exe@gmail.com>
2016-08-18 18:00:56 +0300

    css and js

--- public/window-frames/script.js
+++ public/window-frames/script.js
@@ -1,1 +1,19 @@
+function openPage() {
+  var newWin = window.open('/', 'test window');
+  newWin.document.write(
+    //'<script>window.opener.document.body.innerHTML="test"</script>'
+  );
+
+
+
+}
+
+$(function () {
+  //console.log(document.body.innerHTML);
+  var iframe=document.getElementsByTagName('iframe')[0];
+  var iframeDoc=iframe.contentWindow.document;
+  if(iframeDoc.readyState=='complete'){
+    iframeDoc.body.style.backgroundColor='green';
+  }
+});
 
