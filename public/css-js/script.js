32fee4ca1c0f332f29de23178c6df296521e2926
vetalxpro <vetalpro.exe@gmail.com>
2016-08-20 08:25:28 +0300

    start regexp

--- public/css-js/script.js
+++ public/css-js/script.js
@@ -1,2 +1,14 @@
+function zoomIn(){
+  document.documentElement.style.fontSize=parseInt(getComputedStyle(document.documentElement).fontSize)+2+'px';
+}
+function zoomOut(){
+  document.documentElement.style.fontSize=parseInt(getComputedStyle(document.documentElement).fontSize)-2+'px';
+}
 
-
+function closeLoginForm(){
+  document.getElementById('login-form').style.display='none';
+}
+function showLoginForm(){
+  document.getElementById('login-form').style.display='block';
+  document.forms.loginForm.login.focus();
+}
