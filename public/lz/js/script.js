32945ee23d2212bbf46902b4c4c00b6721cfad12
vetalxpro <vetalpro.exe@gmail.com>
2016-09-07 00:07:09 +0300

    lz finish

--- public/lz/js/script.js
+++ public/lz/js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
   var $courses = $('.courses__list-item');
   $courses.on('click', function () {
-    $(this).addClass('selected');
-    $('.fa.fa-square-o',this)          /*change font-awesome icon*/
+    var curElement = $(this);
+    curElement.addClass('selected');
+    $('.fa.fa-square-o', this)          //change font-awesome icon
         .removeClass('fa-square-o')
         .addClass('fa-check-square');
 
-    $('<div />').css({         /*create green overlay*/
+
+    $('<div />').css({       //create green overlay
       position: 'absolute',
       width: '100%',
       height: '100%',
@@ -14,17 +16,20 @@ $(document).ready(function () {
       top: '0',
       zIndex: '100',
       background: '#8cc34b',
-      opacity:'0.2'
-    }).appendTo($(this).css('position', 'relative'));   /* append to courses__list-item*/
+      opacity: '0.2'
+    }).appendTo(curElement.css('position', 'relative'));      //append  overlay to courses__list-item
 
-    $(this).delay(1000).fadeOut(300);
+
+    curElement.delay(1000).fadeOut(300);
 
     setTimeout(function () {
+      curElement.remove();
+
       if (!$('.courses__list li:visible').length) {
-        $('.courses-title, .courses-subtitle').css({visibility:'hidden'});
+        $('.courses-title, .courses-subtitle').css({visibility: 'hidden'});
         $('.complete').fadeIn(800);
       }
-    }, 1500);
+    }, 1305);
 
   });
 });
